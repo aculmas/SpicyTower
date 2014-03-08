1fd5a858d4da735e0d34c8d77d4a792d83ede089
Martin Haug <mhaug@live.de>
2014-03-08 16:04:56 +0100

    Implemented difficulty setter

--- Row.java
+++ Row.java
@@ -11,14 +11,21 @@ import java.util.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo).
 public class Row
 {
 	public boolean[] objects = new boolean[16];
+	protected boolean oneObj;
     public Row() {
+    	oneObj = false;
     	for (int i = 0; i < objects.length; i++) {
     		if (Math.random() > 0.5) {
     			objects[i] = true;
+    			oneObj = true;
     		}
+    	}
+    	if (oneObj == false) {
+    		objects[8] = true;
     	}
     }
     public Row(double diff) {
+    	oneObj = false;
     	if (diff > 100) {
     		diff = 0.5;
     	} else {
@@ -27,7 +34,11 @@ public class Row
     	for (int i = 0; i < objects.length; i++) {
     		if (Math.random() > diff) {
     			objects[i] = true;
+    			oneObj = true;
     		}
+    	}
+    	if (oneObj == false) {
+    		objects[8] = true;
     	}
     }
 
